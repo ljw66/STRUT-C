ed113fd184be41f288700ef5f3d9c5cb6d1500d5
jwl <58612909+ljw66@users.noreply.github.com>
2025-03-03 12:29:33 +0800

    Add files via upload

---
+++ data/experiment_1/test _code/klib/kmath_km_ks_dist_test.c
@@ -0,0 +1,661 @@
+void test_km_ks_dist_up(void);
+void test_km_ks_dist_down(void);
+
+
+void test_km_ks_dist_up(void){
+
+	aunit_printf("\n##FUNCTION_START_km_ks_dist\n");
+ }
+
+void test_km_ks_dist_down(void){
+	aunit_printf("\n##FUNCTION_END_km_ks_dist\n");
+ }
+
+
+
+void abnormal_km_ks_dist_case(int index);
+
+void abnormal_km_ks_dist_case(int index){
+#ifdef INVALID_CASE
+	INVALID_CASE(index)
+#else
+	aunit_printf("ERROR:case_%d Not Exists\n",index);
+#endif /** INVALID_CASE */
+}
+
+void test_km_ks_dist_1(void){
+	//重复调用计数变量名
+ 	unsigned int aUnItCoUnTeR;
+	//声明参数对应的局部变量，若为基本类型赋默认值
+	int na;
+	double *a;
+	int nb;
+	double *b;
+
+	//声明返回值局部变量，若为基本类型赋默认值
+	double returnValue = 0;
+
+	//值序列相关
+	double *valueList_0[]={(double *)a_PTRTO};
+	unsigned int listLength_0=1;
+	double *valueList_1[]={(double *)b_PTRTO};
+	unsigned int listLength_1=1;
+	int valueList_2[]={0};
+	unsigned int listLength_2=1;
+	int valueList_3[]={0};
+	unsigned int listLength_3=1;
+	//值个数不够时是否沿用最后一个值 1=使用 0=不使用
+	unsigned char useLast_0=0;
+	unsigned char useLast_1=0;
+	unsigned char useLast_2=0;
+	unsigned char useLast_3=0;
+	//调用初始化函数
+
+	//为case_count全局变量赋值
+	case_id=1;
+	//初始化各桩函数调用计数器
+	fabs_call_counter = 0;
+
+	aunit_printf("###TEST_TREE_test_km_ks_dist_1,false,0\n");
+	aunit_printf("#CASE_START_test_km_ks_dist_1\n");
+	//循环计数器,为用户变量/返回值赋值
+	for(aUnItCoUnTeR=0;aUnItCoUnTeR<1;aUnItCoUnTeR++){
+		if(aUnItCoUnTeR<listLength_0 || useLast_0){
+			a=(double *)valueList_0[aUnItCoUnTeR<listLength_0?aUnItCoUnTeR:listLength_0-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_1 || useLast_1){
+			b=(double *)valueList_1[aUnItCoUnTeR<listLength_1?aUnItCoUnTeR:listLength_1-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_2 || useLast_2){
+			na=(int)valueList_2[aUnItCoUnTeR<listLength_2?aUnItCoUnTeR:listLength_2-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_3 || useLast_3){
+			nb=(int)valueList_3[aUnItCoUnTeR<listLength_3?aUnItCoUnTeR:listLength_3-1];
+
+		}
+		returnValue = km_ks_dist(na,a,nb,b);
+	}
+	//输出监视的表达式的值,如果用例的该表达式已有预期,则忽略
+	TEST_ASSERT_EQUAL_DOUBLE("km_ks_dist","returnValue",0,returnValue)
+
+	//预期用例中各桩函数调用次数
+	OB_INT_EXPR("fabs","fabs_call_counter",fabs_call_counter)
+
+	aunit_printf("#CASE_END_test_km_ks_dist_1\n");
+}
+
+void test_km_ks_dist_2(void){
+	//重复调用计数变量名
+ 	unsigned int aUnItCoUnTeR;
+	//声明参数对应的局部变量，若为基本类型赋默认值
+	int na;
+	double *a;
+	int nb;
+	double *b;
+
+	//声明返回值局部变量，若为基本类型赋默认值
+	double returnValue = 0;
+
+	//值序列相关
+	double *valueList_0[]={(double *)a_PTRTO};
+	unsigned int listLength_0=1;
+	double *valueList_1[]={(double *)b_PTRTO};
+	unsigned int listLength_1=1;
+	int valueList_2[]={1};
+	unsigned int listLength_2=1;
+	int valueList_3[]={0};
+	unsigned int listLength_3=1;
+	double valueList_4[]={0.5};
+	unsigned int listLength_4=1;
+	//值个数不够时是否沿用最后一个值 1=使用 0=不使用
+	unsigned char useLast_0=0;
+	unsigned char useLast_1=0;
+	unsigned char useLast_2=0;
+	unsigned char useLast_3=0;
+	unsigned char useLast_4=0;
+	//调用初始化函数
+
+	//为case_count全局变量赋值
+	case_id=2;
+	//初始化各桩函数调用计数器
+	fabs_call_counter = 0;
+
+	aunit_printf("###TEST_TREE_test_km_ks_dist_2,false,0\n");
+	aunit_printf("#CASE_START_test_km_ks_dist_2\n");
+	//循环计数器,为用户变量/返回值赋值
+	for(aUnItCoUnTeR=0;aUnItCoUnTeR<1;aUnItCoUnTeR++){
+		if(aUnItCoUnTeR<listLength_0 || useLast_0){
+			a=(double *)valueList_0[aUnItCoUnTeR<listLength_0?aUnItCoUnTeR:listLength_0-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_1 || useLast_1){
+			b=(double *)valueList_1[aUnItCoUnTeR<listLength_1?aUnItCoUnTeR:listLength_1-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_2 || useLast_2){
+			na=(int)valueList_2[aUnItCoUnTeR<listLength_2?aUnItCoUnTeR:listLength_2-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_3 || useLast_3){
+			nb=(int)valueList_3[aUnItCoUnTeR<listLength_3?aUnItCoUnTeR:listLength_3-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_4 || useLast_4){
+			a_PTRTO[0]=(double)valueList_4[aUnItCoUnTeR<listLength_4?aUnItCoUnTeR:listLength_4-1];
+
+		}
+		returnValue = km_ks_dist(na,a,nb,b);
+	}
+	//输出监视的表达式的值,如果用例的该表达式已有预期,则忽略
+	TEST_ASSERT_EQUAL_DOUBLE("km_ks_dist","returnValue",0.5,returnValue)
+
+	//预期用例中各桩函数调用次数
+	OB_INT_EXPR("fabs","fabs_call_counter",fabs_call_counter)
+
+	aunit_printf("#CASE_END_test_km_ks_dist_2\n");
+}
+
+void test_km_ks_dist_3(void){
+	//重复调用计数变量名
+ 	unsigned int aUnItCoUnTeR;
+	//声明参数对应的局部变量，若为基本类型赋默认值
+	int na;
+	double *a;
+	int nb;
+	double *b;
+
+	//声明返回值局部变量，若为基本类型赋默认值
+	double returnValue = 0;
+
+	//值序列相关
+	double *valueList_0[]={(double *)a_PTRTO};
+	unsigned int listLength_0=1;
+	double *valueList_1[]={(double *)b_PTRTO};
+	unsigned int listLength_1=1;
+	int valueList_2[]={0};
+	unsigned int listLength_2=1;
+	int valueList_3[]={1};
+	unsigned int listLength_3=1;
+	double valueList_4[]={0.5};
+	unsigned int listLength_4=1;
+	//值个数不够时是否沿用最后一个值 1=使用 0=不使用
+	unsigned char useLast_0=0;
+	unsigned char useLast_1=0;
+	unsigned char useLast_2=0;
+	unsigned char useLast_3=0;
+	unsigned char useLast_4=0;
+	//调用初始化函数
+
+	//为case_count全局变量赋值
+	case_id=3;
+	//初始化各桩函数调用计数器
+	fabs_call_counter = 0;
+
+	aunit_printf("###TEST_TREE_test_km_ks_dist_3,false,0\n");
+	aunit_printf("#CASE_START_test_km_ks_dist_3\n");
+	//循环计数器,为用户变量/返回值赋值
+	for(aUnItCoUnTeR=0;aUnItCoUnTeR<1;aUnItCoUnTeR++){
+		if(aUnItCoUnTeR<listLength_0 || useLast_0){
+			a=(double *)valueList_0[aUnItCoUnTeR<listLength_0?aUnItCoUnTeR:listLength_0-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_1 || useLast_1){
+			b=(double *)valueList_1[aUnItCoUnTeR<listLength_1?aUnItCoUnTeR:listLength_1-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_2 || useLast_2){
+			na=(int)valueList_2[aUnItCoUnTeR<listLength_2?aUnItCoUnTeR:listLength_2-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_3 || useLast_3){
+			nb=(int)valueList_3[aUnItCoUnTeR<listLength_3?aUnItCoUnTeR:listLength_3-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_4 || useLast_4){
+			b_PTRTO[0]=(double)valueList_4[aUnItCoUnTeR<listLength_4?aUnItCoUnTeR:listLength_4-1];
+
+		}
+		returnValue = km_ks_dist(na,a,nb,b);
+	}
+	//输出监视的表达式的值,如果用例的该表达式已有预期,则忽略
+	TEST_ASSERT_EQUAL_DOUBLE("km_ks_dist","returnValue",0.5,returnValue)
+
+	//预期用例中各桩函数调用次数
+	OB_INT_EXPR("fabs","fabs_call_counter",fabs_call_counter)
+
+	aunit_printf("#CASE_END_test_km_ks_dist_3\n");
+}
+
+void test_km_ks_dist_4(void){
+	//重复调用计数变量名
+ 	unsigned int aUnItCoUnTeR;
+	//声明参数对应的局部变量，若为基本类型赋默认值
+	int na;
+	double *a;
+	int nb;
+	double *b;
+
+	//声明返回值局部变量，若为基本类型赋默认值
+	double returnValue = 0;
+
+	//值序列相关
+	double *valueList_0[]={(double *)a_PTRTO};
+	unsigned int listLength_0=1;
+	double *valueList_1[]={(double *)b_PTRTO};
+	unsigned int listLength_1=1;
+	int valueList_2[]={2};
+	unsigned int listLength_2=1;
+	int valueList_3[]={2};
+	unsigned int listLength_3=1;
+	double valueList_4[]={0.3};
+	unsigned int listLength_4=1;
+	double valueList_5[]={0.7};
+	unsigned int listLength_5=1;
+	double valueList_6[]={0.7};
+	unsigned int listLength_6=1;
+	double valueList_7[]={0.3};
+	unsigned int listLength_7=1;
+	//值个数不够时是否沿用最后一个值 1=使用 0=不使用
+	unsigned char useLast_0=0;
+	unsigned char useLast_1=0;
+	unsigned char useLast_2=0;
+	unsigned char useLast_3=0;
+	unsigned char useLast_4=0;
+	unsigned char useLast_5=0;
+	unsigned char useLast_6=0;
+	unsigned char useLast_7=0;
+	//调用初始化函数
+
+	//为case_count全局变量赋值
+	case_id=4;
+	//初始化各桩函数调用计数器
+	fabs_call_counter = 0;
+
+	aunit_printf("###TEST_TREE_test_km_ks_dist_4,false,0\n");
+	aunit_printf("#CASE_START_test_km_ks_dist_4\n");
+	//循环计数器,为用户变量/返回值赋值
+	for(aUnItCoUnTeR=0;aUnItCoUnTeR<1;aUnItCoUnTeR++){
+		if(aUnItCoUnTeR<listLength_0 || useLast_0){
+			a=(double *)valueList_0[aUnItCoUnTeR<listLength_0?aUnItCoUnTeR:listLength_0-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_1 || useLast_1){
+			b=(double *)valueList_1[aUnItCoUnTeR<listLength_1?aUnItCoUnTeR:listLength_1-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_2 || useLast_2){
+			na=(int)valueList_2[aUnItCoUnTeR<listLength_2?aUnItCoUnTeR:listLength_2-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_3 || useLast_3){
+			nb=(int)valueList_3[aUnItCoUnTeR<listLength_3?aUnItCoUnTeR:listLength_3-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_4 || useLast_4){
+			b_PTRTO[0]=(double)valueList_4[aUnItCoUnTeR<listLength_4?aUnItCoUnTeR:listLength_4-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_5 || useLast_5){
+			b_PTRTO[1]=(double)valueList_5[aUnItCoUnTeR<listLength_5?aUnItCoUnTeR:listLength_5-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_6 || useLast_6){
+			a_PTRTO[1]=(double)valueList_6[aUnItCoUnTeR<listLength_6?aUnItCoUnTeR:listLength_6-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_7 || useLast_7){
+			a_PTRTO[0]=(double)valueList_7[aUnItCoUnTeR<listLength_7?aUnItCoUnTeR:listLength_7-1];
+
+		}
+		returnValue = km_ks_dist(na,a,nb,b);
+	}
+	//输出监视的表达式的值,如果用例的该表达式已有预期,则忽略
+	TEST_ASSERT_EQUAL_DOUBLE("km_ks_dist","returnValue",0,returnValue)
+
+	//预期用例中各桩函数调用次数
+	OB_INT_EXPR("fabs","fabs_call_counter",fabs_call_counter)
+
+	aunit_printf("#CASE_END_test_km_ks_dist_4\n");
+}
+
+void test_km_ks_dist_5(void){
+	//重复调用计数变量名
+ 	unsigned int aUnItCoUnTeR;
+	//声明参数对应的局部变量，若为基本类型赋默认值
+	int na;
+	double *a;
+	int nb;
+	double *b;
+
+	//声明返回值局部变量，若为基本类型赋默认值
+	double returnValue = 0;
+
+	//值序列相关
+	double *valueList_0[]={(double *)a_PTRTO};
+	unsigned int listLength_0=1;
+	double *valueList_1[]={(double *)b_PTRTO};
+	unsigned int listLength_1=1;
+	int valueList_2[]={2};
+	unsigned int listLength_2=1;
+	int valueList_3[]={2};
+	unsigned int listLength_3=1;
+	double valueList_4[]={0.4};
+	unsigned int listLength_4=1;
+	double valueList_5[]={0.8};
+	unsigned int listLength_5=1;
+	double valueList_6[]={0.6};
+	unsigned int listLength_6=1;
+	double valueList_7[]={0.2};
+	unsigned int listLength_7=1;
+	//值个数不够时是否沿用最后一个值 1=使用 0=不使用
+	unsigned char useLast_0=0;
+	unsigned char useLast_1=0;
+	unsigned char useLast_2=0;
+	unsigned char useLast_3=0;
+	unsigned char useLast_4=0;
+	unsigned char useLast_5=0;
+	unsigned char useLast_6=0;
+	unsigned char useLast_7=0;
+	//调用初始化函数
+
+	//为case_count全局变量赋值
+	case_id=5;
+	//初始化各桩函数调用计数器
+	fabs_call_counter = 0;
+
+	aunit_printf("###TEST_TREE_test_km_ks_dist_5,false,0\n");
+	aunit_printf("#CASE_START_test_km_ks_dist_5\n");
+	//循环计数器,为用户变量/返回值赋值
+	for(aUnItCoUnTeR=0;aUnItCoUnTeR<1;aUnItCoUnTeR++){
+		if(aUnItCoUnTeR<listLength_0 || useLast_0){
+			a=(double *)valueList_0[aUnItCoUnTeR<listLength_0?aUnItCoUnTeR:listLength_0-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_1 || useLast_1){
+			b=(double *)valueList_1[aUnItCoUnTeR<listLength_1?aUnItCoUnTeR:listLength_1-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_2 || useLast_2){
+			na=(int)valueList_2[aUnItCoUnTeR<listLength_2?aUnItCoUnTeR:listLength_2-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_3 || useLast_3){
+			nb=(int)valueList_3[aUnItCoUnTeR<listLength_3?aUnItCoUnTeR:listLength_3-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_4 || useLast_4){
+			b_PTRTO[0]=(double)valueList_4[aUnItCoUnTeR<listLength_4?aUnItCoUnTeR:listLength_4-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_5 || useLast_5){
+			b_PTRTO[1]=(double)valueList_5[aUnItCoUnTeR<listLength_5?aUnItCoUnTeR:listLength_5-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_6 || useLast_6){
+			a_PTRTO[1]=(double)valueList_6[aUnItCoUnTeR<listLength_6?aUnItCoUnTeR:listLength_6-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_7 || useLast_7){
+			a_PTRTO[0]=(double)valueList_7[aUnItCoUnTeR<listLength_7?aUnItCoUnTeR:listLength_7-1];
+
+		}
+		returnValue = km_ks_dist(na,a,nb,b);
+	}
+	//输出监视的表达式的值,如果用例的该表达式已有预期,则忽略
+	TEST_ASSERT_EQUAL_DOUBLE("km_ks_dist","returnValue",0.2,returnValue)
+
+	//预期用例中各桩函数调用次数
+	OB_INT_EXPR("fabs","fabs_call_counter",fabs_call_counter)
+
+	aunit_printf("#CASE_END_test_km_ks_dist_5\n");
+}
+
+void test_km_ks_dist_6(void){
+	//重复调用计数变量名
+ 	unsigned int aUnItCoUnTeR;
+	//声明参数对应的局部变量，若为基本类型赋默认值
+	int na;
+	double *a;
+	int nb;
+	double *b;
+
+	//声明返回值局部变量，若为基本类型赋默认值
+	double returnValue = 0;
+
+	//值序列相关
+	double *valueList_0[]={(double *)a_PTRTO};
+	unsigned int listLength_0=1;
+	double *valueList_1[]={(double *)b_PTRTO};
+	unsigned int listLength_1=1;
+	int valueList_2[]={3};
+	unsigned int listLength_2=1;
+	int valueList_3[]={3};
+	unsigned int listLength_3=1;
+	double valueList_4[]={0.2};
+	unsigned int listLength_4=1;
+	double valueList_5[]={1.0};
+	unsigned int listLength_5=1;
+	double valueList_6[]={0.6};
+	unsigned int listLength_6=1;
+	double valueList_7[]={0.5};
+	unsigned int listLength_7=1;
+	double valueList_8[]={0.1};
+	unsigned int listLength_8=1;
+	double valueList_9[]={0.9};
+	unsigned int listLength_9=1;
+	//值个数不够时是否沿用最后一个值 1=使用 0=不使用
+	unsigned char useLast_0=0;
+	unsigned char useLast_1=0;
+	unsigned char useLast_2=0;
+	unsigned char useLast_3=0;
+	unsigned char useLast_4=0;
+	unsigned char useLast_5=0;
+	unsigned char useLast_6=0;
+	unsigned char useLast_7=0;
+	unsigned char useLast_8=0;
+	unsigned char useLast_9=0;
+	//调用初始化函数
+
+	//为case_count全局变量赋值
+	case_id=6;
+	//初始化各桩函数调用计数器
+	fabs_call_counter = 0;
+
+	aunit_printf("###TEST_TREE_test_km_ks_dist_6,false,0\n");
+	aunit_printf("#CASE_START_test_km_ks_dist_6\n");
+	//循环计数器,为用户变量/返回值赋值
+	for(aUnItCoUnTeR=0;aUnItCoUnTeR<1;aUnItCoUnTeR++){
+		if(aUnItCoUnTeR<listLength_0 || useLast_0){
+			a=(double *)valueList_0[aUnItCoUnTeR<listLength_0?aUnItCoUnTeR:listLength_0-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_1 || useLast_1){
+			b=(double *)valueList_1[aUnItCoUnTeR<listLength_1?aUnItCoUnTeR:listLength_1-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_2 || useLast_2){
+			na=(int)valueList_2[aUnItCoUnTeR<listLength_2?aUnItCoUnTeR:listLength_2-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_3 || useLast_3){
+			nb=(int)valueList_3[aUnItCoUnTeR<listLength_3?aUnItCoUnTeR:listLength_3-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_4 || useLast_4){
+			b_PTRTO[0]=(double)valueList_4[aUnItCoUnTeR<listLength_4?aUnItCoUnTeR:listLength_4-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_5 || useLast_5){
+			b_PTRTO[2]=(double)valueList_5[aUnItCoUnTeR<listLength_5?aUnItCoUnTeR:listLength_5-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_6 || useLast_6){
+			b_PTRTO[1]=(double)valueList_6[aUnItCoUnTeR<listLength_6?aUnItCoUnTeR:listLength_6-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_7 || useLast_7){
+			a_PTRTO[1]=(double)valueList_7[aUnItCoUnTeR<listLength_7?aUnItCoUnTeR:listLength_7-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_8 || useLast_8){
+			a_PTRTO[0]=(double)valueList_8[aUnItCoUnTeR<listLength_8?aUnItCoUnTeR:listLength_8-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_9 || useLast_9){
+			a_PTRTO[2]=(double)valueList_9[aUnItCoUnTeR<listLength_9?aUnItCoUnTeR:listLength_9-1];
+
+		}
+		returnValue = km_ks_dist(na,a,nb,b);
+	}
+	//输出监视的表达式的值,如果用例的该表达式已有预期,则忽略
+	TEST_ASSERT_EQUAL_DOUBLE("km_ks_dist","returnValue",0.1,returnValue)
+
+	//预期用例中各桩函数调用次数
+	OB_INT_EXPR("fabs","fabs_call_counter",fabs_call_counter)
+
+	aunit_printf("#CASE_END_test_km_ks_dist_6\n");
+}
+
+void test_km_ks_dist_7(void){
+	//重复调用计数变量名
+ 	unsigned int aUnItCoUnTeR;
+	//声明参数对应的局部变量，若为基本类型赋默认值
+	int na;
+	double *a;
+	int nb;
+	double *b;
+
+	//声明返回值局部变量，若为基本类型赋默认值
+	double returnValue = 0;
+
+	//值序列相关
+	double *valueList_0[]={(double *)a_PTRTO};
+	unsigned int listLength_0=1;
+	double *valueList_1[]={(double *)b_PTRTO};
+	unsigned int listLength_1=1;
+	int valueList_2[]={3};
+	unsigned int listLength_2=1;
+	int valueList_3[]={3};
+	unsigned int listLength_3=1;
+	double valueList_4[]={0.3};
+	unsigned int listLength_4=1;
+	double valueList_5[]={0.8};
+	unsigned int listLength_5=1;
+	double valueList_6[]={0.5};
+	unsigned int listLength_6=1;
+	double valueList_7[]={0.5};
+	unsigned int listLength_7=1;
+	double valueList_8[]={0.2};
+	unsigned int listLength_8=1;
+	double valueList_9[]={0.7};
+	unsigned int listLength_9=1;
+	//值个数不够时是否沿用最后一个值 1=使用 0=不使用
+	unsigned char useLast_0=0;
+	unsigned char useLast_1=0;
+	unsigned char useLast_2=0;
+	unsigned char useLast_3=0;
+	unsigned char useLast_4=0;
+	unsigned char useLast_5=0;
+	unsigned char useLast_6=0;
+	unsigned char useLast_7=0;
+	unsigned char useLast_8=0;
+	unsigned char useLast_9=0;
+	//调用初始化函数
+
+	//为case_count全局变量赋值
+	case_id=7;
+	//初始化各桩函数调用计数器
+	fabs_call_counter = 0;
+
+	aunit_printf("###TEST_TREE_test_km_ks_dist_7,false,0\n");
+	aunit_printf("#CASE_START_test_km_ks_dist_7\n");
+	//循环计数器,为用户变量/返回值赋值
+	for(aUnItCoUnTeR=0;aUnItCoUnTeR<1;aUnItCoUnTeR++){
+		if(aUnItCoUnTeR<listLength_0 || useLast_0){
+			a=(double *)valueList_0[aUnItCoUnTeR<listLength_0?aUnItCoUnTeR:listLength_0-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_1 || useLast_1){
+			b=(double *)valueList_1[aUnItCoUnTeR<listLength_1?aUnItCoUnTeR:listLength_1-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_2 || useLast_2){
+			na=(int)valueList_2[aUnItCoUnTeR<listLength_2?aUnItCoUnTeR:listLength_2-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_3 || useLast_3){
+			nb=(int)valueList_3[aUnItCoUnTeR<listLength_3?aUnItCoUnTeR:listLength_3-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_4 || useLast_4){
+			b_PTRTO[0]=(double)valueList_4[aUnItCoUnTeR<listLength_4?aUnItCoUnTeR:listLength_4-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_5 || useLast_5){
+			b_PTRTO[2]=(double)valueList_5[aUnItCoUnTeR<listLength_5?aUnItCoUnTeR:listLength_5-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_6 || useLast_6){
+			b_PTRTO[1]=(double)valueList_6[aUnItCoUnTeR<listLength_6?aUnItCoUnTeR:listLength_6-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_7 || useLast_7){
+			a_PTRTO[1]=(double)valueList_7[aUnItCoUnTeR<listLength_7?aUnItCoUnTeR:listLength_7-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_8 || useLast_8){
+			a_PTRTO[0]=(double)valueList_8[aUnItCoUnTeR<listLength_8?aUnItCoUnTeR:listLength_8-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_9 || useLast_9){
+			a_PTRTO[2]=(double)valueList_9[aUnItCoUnTeR<listLength_9?aUnItCoUnTeR:listLength_9-1];
+
+		}
+		returnValue = km_ks_dist(na,a,nb,b);
+	}
+	//输出监视的表达式的值,如果用例的该表达式已有预期,则忽略
+	TEST_ASSERT_EQUAL_DOUBLE("km_ks_dist","returnValue",0.1,returnValue)
+
+	//预期用例中各桩函数调用次数
+	OB_INT_EXPR("fabs","fabs_call_counter",fabs_call_counter)
+
+	aunit_printf("#CASE_END_test_km_ks_dist_7\n");
+}
+
+void aunit_test_func_run(int caseIndex){
+
+	test_km_ks_dist_up();
+
+	switch(caseIndex){
+	case 1:
+		test_km_ks_dist_1();
+		break;
+	case 2:
+		test_km_ks_dist_2();
+		break;
+	case 3:
+		test_km_ks_dist_3();
+		break;
+	case 4:
+		test_km_ks_dist_4();
+		break;
+	case 5:
+		test_km_ks_dist_5();
+		break;
+	case 6:
+		test_km_ks_dist_6();
+		break;
+	case 7:
+		test_km_ks_dist_7();
+		break;
+	default:
+		abnormal_km_ks_dist_case(caseIndex);
+		break;
+	}
+
+	test_km_ks_dist_down();
+}
+
---
+++ data/experiment_1/test _code/klib/kmath_krf_brent_test.c
@@ -0,0 +1,133 @@
+void test_krf_brent_up(void);
+void test_krf_brent_down(void);
+
+
+void test_krf_brent_up(void){
+
+	aunit_printf("\n##FUNCTION_START_krf_brent\n");
+ }
+
+void test_krf_brent_down(void){
+	aunit_printf("\n##FUNCTION_END_krf_brent\n");
+ }
+
+
+
+void abnormal_krf_brent_case(int index);
+
+void abnormal_krf_brent_case(int index){
+#ifdef INVALID_CASE
+	INVALID_CASE(index)
+#else
+	aunit_printf("ERROR:case_%d Not Exists\n",index);
+#endif /** INVALID_CASE */
+}
+
+void test_krf_brent_6(void){
+	//重复调用计数变量名
+ 	unsigned int aUnItCoUnTeR;
+	//声明参数对应的局部变量，若为基本类型赋默认值
+	double x1;
+	double x2;
+	double tol;
+	double (*func)(double, void *);
+	void *data;
+	int *err;
+
+	//声明返回值局部变量，若为基本类型赋默认值
+	double returnValue = 0;
+
+	//值序列相关
+	double valueList_0[]={0.001};
+	unsigned int listLength_0=1;
+	double (*valueList_1[])(double, void *)={(double (*)(double, void *))func_STUB};
+	unsigned int listLength_1=1;
+	int *valueList_2[]={(int *)err_PTRTO};
+	unsigned int listLength_2=1;
+	double valueList_3[]={1.0};
+	unsigned int listLength_3=1;
+	double valueList_4[]={4.0};
+	unsigned int listLength_4=1;
+	int valueList_5[]={0};
+	unsigned int listLength_5=1;
+	int valueList_6[]={0};
+	unsigned int listLength_6=1;
+	//值个数不够时是否沿用最后一个值 1=使用 0=不使用
+	unsigned char useLast_0=0;
+	unsigned char useLast_1=0;
+	unsigned char useLast_2=0;
+	unsigned char useLast_3=0;
+	unsigned char useLast_4=0;
+	unsigned char useLast_5=0;
+	unsigned char useLast_6=0;
+	//调用初始化函数
+
+	//为case_count全局变量赋值
+	case_id=6;
+	//初始化各桩函数调用计数器
+	fabs_call_counter = 0;
+	func_STUB_call_counter = 0;
+	SIGN_call_counter = 0;
+
+	aunit_printf("###TEST_TREE_test_krf_brent_6,false,0\n");
+	aunit_printf("#CASE_START_test_krf_brent_6\n");
+	//循环计数器,为用户变量/返回值赋值
+	for(aUnItCoUnTeR=0;aUnItCoUnTeR<1;aUnItCoUnTeR++){
+		if(aUnItCoUnTeR<listLength_0 || useLast_0){
+			tol=(double)valueList_0[aUnItCoUnTeR<listLength_0?aUnItCoUnTeR:listLength_0-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_1 || useLast_1){
+			func=(double (*)(double, void *))valueList_1[aUnItCoUnTeR<listLength_1?aUnItCoUnTeR:listLength_1-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_2 || useLast_2){
+			err=(int *)valueList_2[aUnItCoUnTeR<listLength_2?aUnItCoUnTeR:listLength_2-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_3 || useLast_3){
+			x1=(double)valueList_3[aUnItCoUnTeR<listLength_3?aUnItCoUnTeR:listLength_3-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_4 || useLast_4){
+			x2=(double)valueList_4[aUnItCoUnTeR<listLength_4?aUnItCoUnTeR:listLength_4-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_5 || useLast_5){
+			err_PTRTO[0]=(int)valueList_5[aUnItCoUnTeR<listLength_5?aUnItCoUnTeR:listLength_5-1];
+
+		}
+		if(aUnItCoUnTeR<listLength_6 || useLast_6){
+			err_PTRTO[1]=(int)valueList_6[aUnItCoUnTeR<listLength_6?aUnItCoUnTeR:listLength_6-1];
+
+		}
+		returnValue = krf_brent(x1,x2,tol,func,data,err);
+	}
+	//输出监视的表达式的值,如果用例的该表达式已有预期,则忽略
+	TEST_ASSERT_EQUAL_DOUBLE("krf_brent","returnValue",0.0,returnValue)
+	TEST_ASSERT_EQUAL_INT("krf_brent","err[0]",0,err[0])
+
+	//预期用例中各桩函数调用次数
+	OB_INT_EXPR("fabs","fabs_call_counter",fabs_call_counter)
+	OB_INT_EXPR("func_STUB","func_STUB_call_counter",func_STUB_call_counter)
+	OB_INT_EXPR("SIGN","SIGN_call_counter",SIGN_call_counter)
+
+	aunit_printf("#CASE_END_test_krf_brent_6\n");
+}
+
+void aunit_test_func_run(int caseIndex){
+
+	test_krf_brent_up();
+
+	switch(caseIndex){
+	case 6:
+		test_krf_brent_6();
+		break;
+	default:
+		abnormal_krf_brent_case(caseIndex);
+		break;
+	}
+
+	test_krf_brent_down();
+}
+
